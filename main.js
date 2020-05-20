f6b90533fea6625fde7031da5c25ee91b4a363c0
michele baruffetti <michelebaruffetti@gmail.com>
2020-05-20 17:18:06 +0200

    approntata richiesta dati in array e idea di come proseguire.

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 	//Code
+
+
+
+    $.ajax({
+        'url' : 'https://flynn.boolean.careers/exercises/api/array/music',
+        'method' : 'GET',
+        'success' : function(data) {
+            var catalogo_dischi = data.response;
+            console.log(catalogo_dischi);
+        },
+        'error' : function() {
+            alert('Le multinazionali della musica ti negano di ascoltare musica immortale')
+        }
+
+        for (var i = 0; i < catalogo_dischi.length; i++) {
+            // all'interno del ciclo for devo fare lavorare handlebar sul template che andrò a predisporre
+        }
+
+    })
+
+
+
 });
